7c11473a05d3f005f41b0fe1792774167f18d238
WILSON <mw0415436@otc.edu>
2018-08-29 16:52:57 -0500

    worked on temperature conversion

--- jsintro.js
+++ jsintro.js
@@ -1,4 +1,8 @@
-let a = 2;
+// Michael Wilson
+
+// predicting final values of problems
+
+/*let a = 2;
 
 a -= 1;
 a++;
@@ -13,4 +17,12 @@ const g = b / a * a; // .... 2/4 = .5?
 
 console.log(a,b,c,d,e,f,g);
 
+*/
+// from celcius to Fahrenheit degree
 
+let c;
+let F; 
+F = [c] * 9/5 + 32;
+
+console.log ("enter a celcius temperature");
+console.log (F);
