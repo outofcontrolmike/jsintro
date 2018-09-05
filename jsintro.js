10b291650efb7400f7a3ce84b9e45fa6c3143b68
WILSON <mw0415436@otc.edu>
2018-09-05 10:36:34 -0500

    Updated Fizz Buzz program
    Signed-off-by: WILSON <mw0415436@otc.edu>

--- jsintro.js
+++ jsintro.js
@@ -27,7 +27,7 @@ F = [c] * 9/5 + 32;
 console.log ("enter a celcius temperature");
 console.log (F);
 
-*/
+
 
 let a;
 a = 10;
@@ -36,3 +36,27 @@ console.log(a * 2);
 console.log(a * 3);
 console.log(a * 4);
 console.log(a * 5);
+
+*/
+
+ 
+for(i=1; i <=100; i++)
+    {
+        if (i % 3 === 0 && i % 5 === 0 )  // If both conditons are true ===
+            {
+                console.log("FizzBuzz")
+            }
+        else if (i % 3 === 0 )   // if modulous 3
+            {
+                console.log("Fizz");
+            }
+        else if (i % 5 === 0 )
+            {
+                console.log("Buzz");  // if modulos 5
+            }
+        else
+            {
+                console.log(i);   // 
+            }
+        }
+
